a10e19063568200f629e83cfe280f0daefa9eed0
Jason Ward <jason.ward@policystat.com>
2014-07-08 12:22:39 -0400

    100% code coverage.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,9 +6,15 @@ from pywkher import generate_pdf
 
 
 class PywkherTestCase(TestCase):
-    def test_generate_pdf(self):
+    def test_generate_pdf_with_html(self):
         pdf_content = generate_pdf(
             html='<html><body>Is this thing on?</body></html>',
+        )
+        assert pdf_content[:4] == '%PDF'
+
+    def test_generate_pdf_with_url(self):
+        pdf_content = generate_pdf(
+            url='http://google.com',
         )
         assert pdf_content[:4] == '%PDF'
 
@@ -17,3 +23,22 @@ class PywkherTestCase(TestCase):
             html=u'<html><body>Schrödinger</body></html>',
         )
         assert pdf_content[:4] == '%PDF'
+
+    def test_no_arguments(self):
+        try:
+            generate_pdf()
+        except ValueError:
+            pass
+        else:
+            raise AssertionError('Should have raised a ValueError')
+
+    def test_too_many_arguments(self):
+        try:
+            generate_pdf(
+                html='foo',
+                url='bar',
+            )
+        except ValueError:
+            pass
+        else:
+            raise AssertionError('Should have raised a ValueError')
